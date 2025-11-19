95087b330d831620021e2d9d3a5ced7dc7d6ad7e
Roman Smirnov <roman.smirnv@gmail.com>
2025-11-19 12:33:15 +0200

    feat: Implement basic app runner

--- src/app.py
+++ src/app.py
@@ -1,3 +1,17 @@
 """
-App entry point stub
+App entry point
 """
+
+from flask import Flask
+from routes import register_routes
+
+
+def create_app() -> Flask:
+    app = Flask(__name__, static_folder="static", static_url_path="")
+    register_routes(app)
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host="0.0.0.0", port=5000, debug=True)  # Debug 
