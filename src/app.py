3166b9641fd6a5325c068c4657747174d1f9e243
Roman Smirnov <roman.smirnv@gmail.com>
2025-11-19 14:46:48 +0200

    fix: Change IP to localhost only

--- src/app.py
+++ src/app.py
@@ -7,11 +7,11 @@ from routes import register_routes
 
 
 def create_app() -> Flask:
-    app = Flask(__name__, static_folder="static", static_url_path="")
+    app = Flask(__name__, static_folder="static")
     register_routes(app)
     return app
 
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=5000, debug=True)  # Debug 
+    app.run(host='127.0.0.1',port=5000, debug=True)
