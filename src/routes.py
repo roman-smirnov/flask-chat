02f18e058ff7d026239e8f9e83d1c1bcf0421f45
Roman Smirnov <roman.smirnv@gmail.com>
2025-11-19 13:20:53 +0200

    feature: Implement get_chat api endpoint

--- src/routes.py
+++ src/routes.py
@@ -3,7 +3,7 @@ from flask import current_app, request, Response
 
 def register_routes(app):
     
-    # Roman: This API is a GET request by default, returns the static HTML
+    # Roman: This endpoint is a GET request by default, returns the static HTML
     @app.route('/')
     def index():
         return app.send_static_file('index.html')
@@ -13,3 +13,14 @@ def register_routes(app):
     @app.route('/<room>')
     def room(room):
         return app.send_static_file('index.html')
+
+    # Roman: This GET endpoint returns the chat log for the room
+    @app.route("/api/chat/<room>", methods=["GET"])
+    def get_chat(room: str):
+        demo_chat = (
+            "[2024-09-10 14:00:51] Roman Smirnov: Hello World! \n"
+            + f"[2024-09-10 14:01:12] Anton Nahhas: Hello room {room}!.\n"
+            + f"[2024-09-10 14:02:51] Roman Smirnov: Hello room {room}!\n"
+            + f"[2024-09-10 14:03:51] Anton Nahhas: Hello room {room}!\n"
+        )
+        return Response(demo_chat, mimetype="text/plain")
