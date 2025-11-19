e49e5ddf2a1d76f561a0be5eec92d9dd13c8b930
Roman Smirnov <roman.smirnv@gmail.com>
2025-11-19 12:32:04 +0200

    feat: Implement serve HTML via root route

--- src/routes.py
+++ src/routes.py
@@ -1,3 +1,10 @@
-"""
-Stub for app endpoints
-"""
+from flask import current_app, request, Response
+
+
+def register_routes(app):
+
+    @app.route("/", defaults={"room": "general"})
+    @app.route("/<room>")
+    def room_page(room: str):
+        """ Returns index.html for any room """
+        return current_app.send_static_file("index.html")
