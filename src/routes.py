f7746de81cd983a7b062b78cea433f52bbae5fd2
Anton <anton-nahhas@hotmail.com>
2025-11-19 13:22:26 +0200

    Add POST route, accept and format message add to data structure

--- src/routes.py
+++ src/routes.py
@@ -1,8 +1,11 @@
 from flask import current_app, request, Response
+from datetime import datetime
 
 
 def register_routes(app):
-    
+
+    chat_rooms = {} # Anton: In-memory storage for chat messages per room
+
     # Roman: This API is a GET request by default, returns the static HTML
     @app.route('/')
     def index():
@@ -13,3 +16,25 @@ def register_routes(app):
     @app.route('/<room>')
     def room(room):
         return app.send_static_file('index.html')
+    
+    # Anton: POST request API, route: '/api/chat/<room>', Accepts a chat message  
+    @app.route('/api/chat/<room>', methods=['POST'])
+    def post_message(room):
+        # Extract user input, add default values in case of not providing one.
+        username = request.form.get('username', 'Anonymous')
+        msg = request.form.get('msg', '')
+
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        formatted_message = f"[{timestamp}] {username}: {msg}"
+
+        # If room does not exist, initialize a new one
+        if room not in chat_rooms:
+            chat_rooms[room] = []
+        
+        # Add message to room
+        chat_rooms[room].append(formatted_message)
+
+        return '', 200
+
+
